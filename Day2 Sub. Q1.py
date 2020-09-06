0e3d6a48b1118bb21ebf168566857fb3858daa67
Shantanu Patil <70842682+shantanu9797@users.noreply.github.com>
2020-09-06 16:43:25 +0530

    Day 2 Assignment Submission

---
+++ Day2 Sub. Q1.py
@@ -0,0 +1,9 @@
+a= int(input("Hey Pilot,Tell me no. of Feet. "))
+
+
+if a==1000:
+    print("Safe to land.")
+elif a<=5000:
+    print("Bring down to 1000 Feet.")
+elif a>5000:
+    print("Turn around plane.")
